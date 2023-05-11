6ea665bd3171d4a89b09cb1e2b24b645dcab5695
smallpine8 <nkomatsu.dev@gmail.com>
2023-05-11 10:22:02 +0900

    testing-libraryを利用したテストを追加

--- src/chapter2/ui/Button.test.tsx
+++ src/chapter2/ui/Button.test.tsx
@@ -1,9 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import renderer from 'react-test-renderer'
+import {render, fireEvent} from '@testing-library/react'
 import { Button } from './Button'
 
 describe('Button', () => {
   it('renders correctly with react-test-renderer', () => {
     const button = renderer.create(<Button />)
     expect(button.toJSON()).toMatchSnapshot()
+  })
+
+  it('changes the button text upon clicking the button using React Testing Library', () => {
+    const button = render(<Button/>)
+
+    fireEvent.click(button.getByText('ON'))
+    expect(button.getByText(/OFF/i)).toBeTruthy()
+
+    fireEvent.click(button.getByText('OFF'))
+    expect(button.getByText(/ON/i))
   })
 })
