7afdd25ebb468cfccab1e9df609446bee79b5281
smallpine8 <nkomatsu.dev@gmail.com>
2023-05-19 10:00:04 +0900

    Buttonのstoryを追加

---
+++ src/chapter2/ui/Button.stories.tsx
@@ -0,0 +1,5 @@
+import {ComponentMeta, ComponentStoryObj} from '@storybook/react'
+import {Button} from './Button'
+
+export default {component: Button} as  ComponentMeta<typeof Button>
+export const Primary: ComponentStoryObj<typeof Button> = {}
